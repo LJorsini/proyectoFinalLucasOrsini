3c654b3ba15c8dc285d36a06ce72a95a1b1c1b16
Lucas Orsini <lucasorsini@gmail.com>
2024-01-17 20:27:00 -0300

    Add project files.

---
+++ EntregableLucasOrsini/Venta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntregableLucasOrsini
+{
+    internal class Venta
+    {
+        private int _id;
+        private string _comentarios;
+        private int _idUsuario;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Comnetarios
+        {
+            get
+            {
+                return _comentarios;
+            }
+            set
+            {
+                _comentarios = value;
+            }
+        }
+
+        public int IdUsuario
+        {
+            get
+            {
+                return _idUsuario;
+            }
+            set
+            {
+                _idUsuario = value;
+            }
+        }
+    }
+}
